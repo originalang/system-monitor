bc4507697b2dc9b8732a0e8b667641bc0bf359fa
originalang <angeorge200@gmail.com>
2019-12-18 02:51:40 -0500

    Add styles to the temperature display template

--- dashboard/index.js
+++ dashboard/index.js
@@ -10,9 +10,6 @@ app.use(express.static(__dirname + '/public'));
 
 const temperature_url = "http://nginx:80/temperature";
 
-
-const temp_data = [{"type": "acpitz", "temperature_sensors": [{"label": "", "current": 25.0, "high": 107.0, "critical": 107.0}]}, {"type": "pch_skylake", "temperature_sensors": [{"label": "", "current": 32.5, "high": null, "critical": null}]}, {"type": "coretemp", "temperature_sensors": [{"label": "Package id 0", "current": 37.0, "high": 100.0, "critical": 100.0}, {"label": "Core 0", "current": 36.0, "high": 100.0, "critical": 100.0}, {"label": "Core 1", "current": 37.0, "high": 100.0, "critical": 100.0}, {"label": "Core 2", "current": 36.0, "high": 100.0, "critical": 100.0}, {"label": "Core 3", "current": 36.0, "high": 100.0, "critical": 100.0}]}]
-
 app.get('/', (req, res) => {
 	request({
 		url: temperature_url,
